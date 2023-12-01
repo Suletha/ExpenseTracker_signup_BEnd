688e43222fef21c312ecb57a46b0c80819f25d1c
suletha <suletha.gopi@gmail.com>
2023-12-02 01:11:51 +0530

    table_created

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 const bcrypt = require('bcrypt');
 
-const sequelize=new Sequelize('node-complete','root','Krishna',{
+const sequelize=new Sequelize('node-complete','root','Krishna@2304',{
     dialect:'mysql',
     host: 'localhost'
 });

4a51c263ecd9906886c27bbf6ff259db6fda9b05
suletha <suletha.gopi@gmail.com>
2023-12-07 14:02:22 +0530

    report_gen_BE

--- routes/purchase-routes.js
+++ routes/purchase-routes.js
@@ -7,8 +7,22 @@ const router = express.Router();
 // /purchase/premiummembership => POST
 router.get("/premiummembership", purchaseController.purchasePremium);
 
+// purchase/updatetransactionstatus => POST
+router.post(
+  "/updatetransactionstatus",
+  purchaseController.updatetransactionstatus
+);
+
+// purchase/failedtransactionstatus => POST
+router.post(
+  "/failedtransactionstatus",
+  purchaseController.failedtransactionstatus
+);
+
+// /purchase/checkpremium => GET
+router.get("/checkpremium", purchaseController.checkPremium);
+
 // /purchase/getleaderboard =>
 router.get("/getleaderboard", purchaseController.getLeaderBoard);
-
 
 module.exports = router;

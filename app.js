d0db31149d8b9e3a391d13e6cff68eba87c54e94
suletha <suletha.gopi@gmail.com>
2023-12-07 16:57:49 +0530

    go_live

--- app.js
+++ app.js
@@ -11,11 +11,15 @@ const sequelize = require("./util/database");
 const userRoutes = require("./routes/user-routes");
 const expenseRoutes = require("./routes/expense-routes");
 const purchaseRoutes = require("./routes/purchase-routes");
+const passwordRoutes = require("./routes/password-routes");
 const authenticationMiddleware = require("./util/authentication");
+
 
 const User = require("./models/user");
 const Expense = require("./models/expense");
 const Order = require("./models/order");
+const ForgetpasswordRequest = require("./models/forgetpasswordrequest");
+const DownloadedFile = require("./models/filesdownloaded");
 
 
 const app = express();
@@ -32,7 +36,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //app.use(express.static(path.join(__dirname, "public")));
 
-
+app.use("/password", passwordRoutes);
 app.use("/users", userRoutes);
 app.use("/purchase", authenticationMiddleware, purchaseRoutes);
 app.use(expenseRoutes);
@@ -42,6 +46,12 @@ Expense.belongsTo(User);
 
 User.hasMany(Order);
 Order.belongsTo(User);
+
+User.hasMany(ForgetpasswordRequest);
+ForgetpasswordRequest.belongsTo(User);
+
+User.hasMany(DownloadedFile);
+DownloadedFile.belongsTo(User);
 
 
 
--- util/database.js
+++ util/database.js
@@ -1,9 +1,13 @@
-const { Sequelize } = require('sequelize');
-const bcrypt = require('bcrypt');
+const Sequelize = require("sequelize");
 
-const sequelize=new Sequelize('node-complete','root','Krishna@2304',{
-    dialect:'mysql',
-    host: 'localhost'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+  }
+);
 
-module.exports=sequelize;
+module.exports = sequelize;
